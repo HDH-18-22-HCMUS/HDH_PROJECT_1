96bf4c652dcda6dcda0b63db9ac303125de30c75
Minh Pham <2393780@gmail.com>
2020-11-10 11:23:03 +0700

    Modify syscalls to advance program counter

--- nachos/nachos-3.4/code/userprog/exception.cc
+++ nachos/nachos-3.4/code/userprog/exception.cc
@@ -1,4 +1,4 @@
-// exception.cc 
+// exception.cc
 //	Entry point into the Nachos kernel from user programs.
 //	There are two kinds of things that can cause control to
 //	transfer back to here from user code:
@@ -9,7 +9,7 @@
 //
 //	exceptions -- The user code does something that the CPU can't handle.
 //	For instance, accessing memory that doesn't exist, arithmetic errors,
-//	etc.  
+//	etc.
 //
 //	Interrupts (which can also cause control to transfer from user
 //	code into the Nachos kernel) are handled elsewhere.
@@ -18,20 +18,18 @@
 // Everything else core dumps.
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
+// All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
 #include "copyright.h"
 #include "system.h"
 #include "syscall.h"
 
-
 // To not show error
 //#include "synchcons.h"
-//SynchConsole gSynchConsole;
+SynchConsole *gSynchConsole;
 
-//#include "machine.h"
-//Machine* machine;
+#include "machine.h"
 //----------------------------------------------------------------------
 // ExceptionHandler
 // 	Entry point into the Nachos kernel.  Called when a user program
@@ -46,68 +44,78 @@
 //		arg3 -- r6
 //		arg4 -- r7
 //
-//	The result of the system call, if any, must be put back into r2. 
+//	The result of the system call, if any, must be put back into r2.
 //
 // And don't forget to incremenSt the pc before returning. (Or else you'll
 // loop making the same system call forever!
 //
-//	"which" is the kind of exception.  The list of possible exceptions 
+//	"which" is the kind of exception.  The list of possible exceptions
 //	are in machine.h.
 //----------------------------------------------------------------------
 
-void
-ExceptionHandler(ExceptionType which)
+void AdvanceProgramCounter(int pcAfter)
 {
-    //int type = machine->ReadRegister(2);
-    int type = SC_Sub;
+    // Advance program counters.
+    machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+    // for debugging, in case we
+    // are jumping into lala-land
+    machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+    machine->WriteRegister(NextPCReg, pcAfter);
+}
+
+void ExceptionHandler(ExceptionType which)
+{
+    int type = machine->ReadRegister(2);
+    int pcAfter = machine->ReadRegister(NextPCReg) + 4;
+    //int type = SC_Sub;
+    /*
+    Khong goi interrupt->Halt() nua
+    Thay bang ham AdvanceProgramCounter();
+    Goi ham nay truoc khi return syscall
+    */
     switch (which)
     {
-        case NoException:
-            return;
-        case SyscallException:
-            switch (type)
-            {
-            case SC_Halt:
-            {
-                DEBUG('a', "Shutdown, initiated by user program.\n");
-   	            interrupt->Halt();
-                break;
-            }
-            //test SC_ sub
-            case SC_Sub:
-            {
-                int op1 = machine->ReadRegister (4);
-                int op2 = machine->ReadRegister (5);
-                int result = op1 - op2;
-                machine->WriteRegister (2, result);
-                //DEBUG('a', "123dfhh.\n");
-                interrupt->Halt();
-                break; 
-            }
-
+    case NoException:
+        return;
+    case SyscallException:
+        switch (type)
+        {
+        case SC_Halt:
+        {
+            DEBUG('a', "Shutdown, initiated by user program.\n");
+            interrupt->Halt();
+            break;
+        }
+        //test SC_ sub
+        case SC_Sub:
+        {
             
-            case SC_ReadChar:
-            {
-                //char ch = new char;
-                //int numB = gSynchConsole.Read(ch,1);
-                //machine->WriteRegister(2,ch);
-                interrupt->Halt();
-                break;
-            }
-            
-            case SC_PrintChar:
-            {
-                //char ch = machine->ReadRegister(4);
-                //gSynchConsole.Write(ch,1);
-                //machine->WriteRegister(2,0);
-                interrupt->Halt();
-                break;    
-            }
-            default:
-                printf("Unexpected user mode exception %d %d\n", which, type);
-                interrupt->Halt();
-                break;
-            }
+            AdvanceProgramCounter(pcAfter);
+            break;
+        }
 
+        case SC_ReadChar:
+        {
+            
+            AdvanceProgramCounter(pcAfter);
+            break;
+        }
+
+        case SC_PrintChar:
+        {
+            
+            AdvanceProgramCounter(pcAfter);
+            break;
+        }
+        case SC_ReadInt:
+        {
+            AdvanceProgramCounter(pcAfter);
+            break;
+        }
+        default:
+            printf("Unexpected user mode exception %d %d\n", which, type);
+            interrupt->Halt();
+            break;
+        }
     }
 }
