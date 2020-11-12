a837edf72eb641e99dfb8a366121c33c73d4ac1a
YennNgoc <ngocvy.nts@gmail.com>
2020-11-12 20:23:28 +0700

    done test char and push help

---
+++ nachos/nachos-3.4/code/test/help.c
@@ -0,0 +1,16 @@
+#include "syscall.h"
+#include "copyright.h"
+int main()
+{
+	PrintString("DO AN HE DIEU HANH K18");
+    PrintString("18120209 Pham Nhat Minh");
+    PrintString("18120214 Le Ngoc Bao Ngan");
+    PrintString("18120215 Vu Yen Ngoc");
+    PrintString("18120217 Nguyen Tran Ai Nguyen");
+    PrintString("18120223 Nguyen Ha Dai Thien Phu");
+	PrintString("MO TA CHUONG TRINH");
+    PrintString("ASCII: Chuong trinh thuc hien in ra bang ma Ascii");
+    PrintString("SORT: Thuc hien thuat toan Bubble Sort");
+    
+    return 0;
+}
