86fa1a8eec2fea7517f9eb75564cc6eac8a7bdc8
Minh Pham <2393780@gmail.com>
2020-11-16 12:05:18 +0700

    Revert "Merge branch 'glugluglu'"
    This reverts commit cec38db4976cb36a4f9d52a14819547197bb0ba6, reversing
    changes made to 0655d4e3e5cfa3111b90ecee39e50a3b9c2d356e.

--- nachos/nachos-3.4/code/test/char.c
+++ nachos/nachos-3.4/code/test/char.c
@@ -2,18 +2,8 @@
 #include "copyright.h"
 int main()
 {
-	char a[10];
-	int i;
-	for (i = 0; i < 5; i++)
-	{
-		a[i] = ReadChar();
-	}
-
-	for (i = 0; i < 5; i++)
-	{
-		PrintChar(a[i]);
-		PrintChar(' ');
-	}
-
+	char c = ReadChar();
+	//char c= 'e';
+	PrintChar(c);
 	return 0;
 }
--- nachos/nachos-3.4/code/test/int.c
+++ nachos/nachos-3.4/code/test/int.c
@@ -7,12 +7,22 @@ int main()
 {
     
 	int a, i;
-    //PrintString("Nhap a: ");
-    a = ReadInt();
-    //a = -456;
-    PrintString("=> a = ");
-    PrintInt(a);
+   PrintString("Nhap a: ");
+   a = ReadInt();
+   PrintString("Nhap mang: \n");
    
+   for (i = 0; i < a; i++)
+   {
+      A[i] = ReadInt();
+   }
+   PrintString("\n");
+
+   for (i = 0; i < a; i++)
+   {
+      PrintInt(A[i]);
+      PrintChar(' ');
+   }
+   PrintString("\n");
     
 	return 0;
 }
